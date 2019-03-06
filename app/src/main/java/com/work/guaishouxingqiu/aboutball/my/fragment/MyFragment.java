4013951f7c2d5f6d1d39fb3c615217e313535b64
zzf <370140870@qq.com>
2019-03-06 18:34:22 +0800

    第一次创建项目，搭建框架

--- app/src/main/java/com/work/guaishouxingqiu/aboutball/my/fragment/MyFragment.java
+++ app/src/main/java/com/work/guaishouxingqiu/aboutball/my/fragment/MyFragment.java
@@ -17,7 +17,8 @@ public class MyFragment extends BaseFragment<MyPresenter>implements MyContract.V
     }
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_my;
+        return
+                R.layout.fragment_my;
     }
 
     @Override
